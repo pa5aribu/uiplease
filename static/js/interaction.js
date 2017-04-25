799f48a5c8c1da11b1e95d717f0ec67641b3572d
Bhakti Al Akbar <balapa95@gmail.com>
2017-04-25 08:38:36 +0700

    fixed input bug

--- static/js/interaction.js
+++ static/js/interaction.js
@@ -24,12 +24,6 @@ inputEmail.addEventListener("keyup", function(e){
 	
 });
 
-inputEmail.addEventListener("blur", function(e){
-
-	this.className = "";
-
-});
-
 function validateEmail(email) {
   var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(email);
